d43978c0074df637a4f8bba7d0871750995c0a52
david colvin <davidmcolvin@gmail.com>
2020-01-10 20:45:17 -0500

    Complete the challenge

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -14,9 +14,39 @@ namespace ConsoleUI
       PaymentProcessor paymentProcessor = new PaymentProcessor();
       for (int i = 0; i <= 10; i++)
       {
-        var result = paymentProcessor.MakePayment($"Demo{ i }", i);
+        TransactionModel result;
+        try
+        {
+          result = paymentProcessor.MakePayment($"Demo{ i }", i);
 
-        Console.WriteLine(result.TransactionAmount);
+          if (result != null)
+          {
+            Console.WriteLine(result.TransactionAmount);
+          }
+          else
+          {
+            Console.WriteLine($"Null value for item {i}");
+          }
+        }
+        catch (IndexOutOfRangeException ex)
+        {
+          Console.WriteLine("Skipped invalid record");
+        }
+        catch (FormatException ex) when (i != 5)
+        {
+          var message = "Formatting Issue";
+          Exception myEx = ex.InnerException;
+          while (myEx != null)
+          {
+            message += " " + myEx.Message;
+            myEx = myEx.InnerException;
+          }
+          Console.WriteLine(message);
+        }
+        catch (Exception)
+        {
+          Console.WriteLine($"Payment skipped for payment with {i} items");
+        }
       }
       Console.ReadLine();
     }
